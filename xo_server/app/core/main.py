a180ca9b1627dc14bfb218a3bedf04984a488e1e
ForestWildRabbit <krabovedok@mail.ru>
2025-08-14 22:02:50 +0300

    CORS enabled

--- xo_server/app/core/main.py
+++ xo_server/app/core/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, APIRouter
 from app.game.routers import game_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(root_path="/api")
 
 base_router = APIRouter()
 base_router.include_router(game_router)
 app.include_router(base_router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
