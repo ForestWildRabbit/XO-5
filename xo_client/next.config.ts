e006c91c9cd9c5feb3b41e12492476c50b1bba94
ForestWildRabbit <krabovedok@mail.ru>
2025-08-14 19:46:28 +0300

    Next JS Dockerfile

--- xo_client/next.config.ts
+++ xo_client/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+    // ... rest of the configuration.
+    output: "standalone",
+};
+
 const nextConfig: NextConfig = {
-    /* config options here */
     devIndicators: false,
 };
 
